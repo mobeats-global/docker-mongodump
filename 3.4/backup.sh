e24dda34964263d212b79c7953cbb7d9e5d4d8df
Juan Gimenez <jgimenez@mobeats.com.ar>
2019-01-09 08:58:33 -0300

    rollback change

--- 3.4/backup.sh
+++ 3.4/backup.sh
@@ -5,11 +5,12 @@ set -e
 echo "Job started: $(date)"
 
 DATE=$(date +%Y%m%d_%H%M%S)
-filename=$NAME
+FILE="/backup/backup-$DATE.tar.gz"
 
 mkdir -p dump
 mongodump -h $MONGO_HOST -p $MONGO_PORT
-tar -zcvf /backup/$filename.tar.gz dump/
+tar -zcvf $FILE dump/
+
 rm -rf dump/
 
 echo "Job finished: $(date)"
