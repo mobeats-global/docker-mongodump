78e584acce6da11681316033705db83a9d533531
Juan <jgimenez@mobeats.com.ar>
2019-04-16 17:30:22 -0300

    Added 3.6 support

---
+++ 3.6/backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+echo "Job started: $(date)"
+
+DATE=$(date +%Y%m%d_%H%M%S)
+FILE="/backup/backup-$DATE.tar.gz"
+
+mkdir -p dump
+mongodump -h $MONGO_HOST -p $MONGO_PORT $EXTRA_OPTS
+tar -zcvf $FILE dump/
+
+rm -rf dump/
+
+echo "Job finished: $(date)"
